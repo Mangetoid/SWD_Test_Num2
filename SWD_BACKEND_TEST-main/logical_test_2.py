90a9bbc19e7b8183127cd458d64a15175dce713f
Sarawuth <sarawuthwongsanga@gmail.com>
2023-06-10 02:23:51 +0700

    Add logical_test_2

--- SWD_BACKEND_TEST-main/logical_test_2.py
+++ SWD_BACKEND_TEST-main/logical_test_2.py
@@ -8,3 +8,37 @@ Convert Arabic Number to Roman Number.
 ห้ามใช้ Library อื่น ๆ ที่ต้อง import ในการทำงาน(ยกเว้น ใช้เพื่อการ test การทำงานของฟังก์ชัน).
 
 """
+def number_to_roman(number):
+    roman_numerals = {
+        1000: 'M',
+        900: 'CM',
+        500: 'D',
+        400: 'CD',
+        100: 'C',
+        90: 'XC',
+        50: 'L',
+        40: 'XL',
+        10: 'X',
+        9: 'IX',
+        5: 'V',
+        4: 'IV',
+        1: 'I'
+    }
+
+    roman_number = ''
+    for value, symbol in roman_numerals.items():
+        while number >= value:
+            roman_number += symbol
+            number -= value
+
+    return roman_number
+
+
+# Get input from the user
+number = int(input("Enter an integer: "))
+
+# Convert the number to Roman numeral
+roman_number = number_to_roman(number)
+
+# Print the result
+print("Roman Numeral:", roman_number)
